091788eb1c3dcaf181a8c65d1749c0586de451e9
Rebecca Pellegran <rebeccapellegran@gmail.com>
2020-10-15 11:54:17 -0400

    create categories

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const letters= "abcdefghjiklmnopqrstuvwxyz";
 let lettersArray = Array.from(letters);
 //creates an array from letters 
 
-console.log(lettersArray);
+//console.log(lettersArray);
 //testing to see that the letters created the array
 
 let lettersClass = document.querySelector('.letters');
@@ -23,6 +23,23 @@ lettersArray.forEach(letter => {
 })
 //Generates the letters for Hangman!
 //OMG IT WORKEDD!!!!!
+
+const words = {
+    animals: ["cat", "camel", "toucan","elephant","dog","giraffe","snake","lion","panda"],
+    movies: ["Pulp Fiction","Lord of the Rings","The Hobbit","The Gentlemen","Interstellar","A Beautiful Mind", "Imagination Game", "Monty Python"],
+    countries: ["Israel","Norway","Egypt","Honduras","Palestine","Paraguay","Brazil","Jordan","Netherlands","Germany","Greece","Bosnia","Ukraine"],
+    people: ["Angelina Jolie", "Albert Einstein", "Jack the Ripper","Kylie Jenner"]
+}
+// populate the categories keys
+let allKeys = Object.keys(words);
+//console.log(allKeys)
+// 
+
+let randomnPropNumber = Math.floor(Math.random()* allKeys.length);
+//console.log(randomnPropNumber)
+// get randomn category hopefully from words which is allkeys now
+
+
 
 
 
